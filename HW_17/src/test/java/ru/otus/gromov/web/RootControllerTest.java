0241637a1b1ed7b60efd90376728dde313019de1
grom <mr.gromov.vitaly@gmail.com>
2018-11-21 16:35:21 +0300

    HW16 :: Docker

---
+++ HW_17/src/test/java/ru/otus/gromov/web/RootControllerTest.java
@@ -0,0 +1,102 @@
+package ru.otus.gromov.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.annotation.PostConstruct;
+
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
+public class RootControllerTest {
+
+    private static final CharacterEncodingFilter CHARACTER_ENCODING_FILTER = new CharacterEncodingFilter();
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+
+    private MockMvc mockMvc;
+
+    static {
+        CHARACTER_ENCODING_FILTER.setEncoding("UTF-8");
+        CHARACTER_ENCODING_FILTER.setForceEncoding(true);
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .addFilter(CHARACTER_ENCODING_FILTER)
+                .build();
+    }
+
+    @Test
+    public void indexPage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void listAuthors() throws Exception {
+        mockMvc.perform(get("/authors"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void viewAuthor() throws Exception {
+        long id = 1L;
+        mockMvc.perform(get("/authors/" + id))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void listBook() throws Exception {
+        mockMvc.perform(get("/books"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void viewBook() throws Exception {
+        long id = 1L;
+        mockMvc.perform(get("/authors/" + id))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void listGenre() throws Exception {
+        long id = 1L;
+        mockMvc.perform(get("/genres"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void viewGenre() throws Exception {
+        long id = 1L;
+        mockMvc.perform(get("/genres/" + id))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
